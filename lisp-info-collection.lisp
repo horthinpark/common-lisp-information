cd6a4da2a35f19fa3875a89ecc6d748407dccf55
horthinpark <pent23tkms@gmail.com>
2015-04-24 22:54:47 +0900

    stack-over-flow,qiitaのlisp情報を時系列順にconsole上に表示

--- lisp-info-collection.lisp
+++ lisp-info-collection.lisp
@@ -8,6 +8,30 @@
 
 ; ボディを文字列で取得するために、テキストとして判定される Content-Type を追加
 (pushnew '("application" . "json") drakma:*text-content-types* :test #'equal)
+
+(defvar *unix-epoch-difference*
+    (encode-universal-time 0 0 0 1 1 1970 0))
+
+(defun unix-to-universal-time (unix-time)
+    (+ unix-time *unix-epoch-difference*))
+
+(defparameter *article-bucket* (make-array 5 :fill-pointer 0 :adjustable t))
+
+(defun date-to-universal-time (date)
+  (encode-universal-time (parse-integer (subseq date 17 19)) (parse-integer (subseq date 14 16)) (parse-integer (subseq date 11 13)) (parse-integer (subseq date 8 10)) (parse-integer (subseq date 5 7)) (parse-integer (subseq date 0 4)) 9))
+
+(defun universal-time-to-date (universal_time)
+  (multiple-value-bind (sec min hr day mon year dow daylight-p zone)
+      (decode-universal-time universal_time)
+    (declare (ignore daylight-p zone))
+    (format nil "~[Mon~;Tue~;Wed~;Thu~;Fri~;Sat~;Sun~] ~d ~[Jan~;Feb~;Mar~;Apr~;May~;Jun~;Jul~;Aug~;Sep~;Oct~;Nov~;Dec~] ~d ~2,'0d:~2,'0d:~2,'0d"
+            dow
+            day
+            (1- mon)
+            year
+            hr min sec)))
+
+;------qiita---------
 (multiple-value-bind (body status)
     (drakma:http-request "https://qiita.com/api/v1/tags/common-lisp/items")
   (when (= status 200)
@@ -18,29 +42,25 @@
        (qiita-article-array (yason:parse *qiita-json-array*)))
   (defparameter *qiita-article-array* qiita-article-array))
 
-(defparameter *article-bucket* (make-array 5 :fill-pointer 0 :adjustable t))
-
 ;qiita-article-arrayから必要な情報を抽出
 (loop for article across *qiita-article-array* do
   (let* ((id (gethash "id" article))
-        (updated_at (gethash "updated_at" article))
-        (url (gethash "url" article))
-        (title (gethash "title" article))
-        (user (gethash "user" article))
-        (user_name (gethash "url_name" user))
-         (universal_updated_at (encode-universal-time (parse-integer (subseq updated_at 17 19)) (parse-integer (subseq updated_at 14 16)) (parse-integer (subseq updated_at 11 13)) (parse-integer (subseq updated_at 8 10)) (parse-integer (subseq updated_at 5 7)) (parse-integer (subseq updated_at 0 4)) 9)))
-      (vector-push-extend (alexandria:plist-hash-table '( "article-id" id "updated_at" universal_updated_at "url" url "title" title "user_name" user_name) :test #'equal) *article-bucket*)
-      (print (alexandria:plist-hash-table '( "article-id" id "updated_at" universal_updated_at "url" url "title" title "user_name" user_name) :test #'equal)))))
-   ; (format t "article-id:  ~A~%user: ~A~%title: ~A~%update_at: ~A~%url: ~A~%~%~%" id user_name title universal_updated_at url))))
+         (updated_at (gethash "updated_at" article))
+         (url (gethash "url" article))
+         (title (gethash "title" article))
+         (user (gethash "user" article))
+         (user_name (gethash "url_name" user))
+         (article_table ())
+         (universal_updated_at (date-to-universal-time updated_at)))
+    (setf (getf article_table :article_id) id)
+    (setf (getf article_table :updated_at) universal_updated_at)
+    (setf (getf article_table :url) url)
+    (setf (getf article_table :title) title)
+    (setf (getf article_table :author_name) user_name)
+    (vector-push-extend article_table *article-bucket*)))
 
 
-(let ((a 1) (b 2) (c 3))
-  (print a))
-
-
-
-(print (gethash "title" *article-bucket*))
-
+;-------stackoverflow-----------
 (multiple-value-bind (body status)
     (drakma:http-request "https://api.stackexchange.com/2.2/questions?page=1&pagesize=50&order=desc&sort=activity&tagged=common-lisp&site=stackoverflow")
   (when (= status 200)
@@ -49,11 +69,30 @@
 (defparameter *stackoverflow-articles-array* (gethash "items" (yason:parse *stackoverflow-json-array*)))
 
 (loop for article in *stackoverflow-articles-array* do
+  (print article)
      (let ((owner (gethash "owner" article))
+           (id (gethash "question_id" article))
            (is_answered (gethash "is_answered" article))
            (last_activity_date (gethash "last_activity_date" article))
            (link (gethash "link" article))
-           (title (gethash "title" article)))
+           (title (gethash "title" article))
+           (article_table ()))
        (let ((author_name (gethash "display_name" owner)))
-         (format t "updated_at: ~A~%title: ~A~%~%author: ~A~%is_answered: ~a~%url: ~A~%" last_activity_date title author_name is_answered link))))
-       
+         (setf (getf article_table :article_id) id)
+         (setf (getf article_table :updated_at) (unix-to-universal-time last_activity_date))
+         (setf (getf article_table :url) link)
+         (setf (getf article_table :title) title)
+         (setf (getf article_table :author_name) author_name)
+    (vector-push-extend article_table *article-bucket*))))
+
+;---sort-----
+(defparameter *sorted_article_bucket*  (sort *article-bucket* #'> :key #'(lambda (article) (getf article :updated_at))))
+
+;------display---------
+(loop for article across *sorted_article_bucket* do
+  (let* ((universal_updated_at (getf article :updated_at))
+        (url (getf article :url))
+        (title (getf article :title))
+         (author_name (getf article :author_name))
+         (updated_at (universal-time-to-date universal_updated_at)))
+    (format t "update_at: ~A~%user: ~A~%title: ~A~%url: ~A~%~%~%" updated_at author_name title url)))
